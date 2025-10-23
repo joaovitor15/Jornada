75bdd563c5fec28a6e10d6dcb0eb5f1e50c220f5
João Vitor Machry <joaovitormachry@gmail.com>
2025-10-23 21:14:35 +0000

    TESTANDO

--- src/components/ui/currency-input.tsx
+++ src/components/ui/currency-input.tsx
@@ -1,12 +1,16 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { NumericFormat, type OnValueChange, type NumericFormatProps } from "react-number-format";
-import { Input } from "@/components/ui/input";
+import * as React from 'react';
+import {
+  NumericFormat,
+  type OnValueChange,
+} from 'react-number-format';
+
+import { Input } from '@/components/ui/input';
 
 type CurrencyInputProps = Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
-  "prefix"
+  'prefix'
 > & {
   onValueChange?: OnValueChange;
 };
@@ -14,15 +18,16 @@ type CurrencyInputProps = Omit<
 const CurrencyInput = React.forwardRef<HTMLInputElement, CurrencyInputProps>(
   ({ onChange, onValueChange, onFocus, ...props }, ref) => {
     const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    const inputElement = e.target;
-    const length = inputElement.value.length;
-    setTimeout(() => {
-      inputElement.setSelectionRange(length, length);
-    }, 0);
-    if (onFocus) {
-    onFocus(e);
-    }
-  };
+      const inputElement = e.target;
+      const length = inputElement.value.length;
+      setTimeout(() => {
+        inputElement.setSelectionRange(length, length);
+      }, 0);
+      if (onFocus) {
+        onFocus(e);
+      }
+    };
+
     return (
       <NumericFormat
         customInput={Input}
@@ -31,24 +36,29 @@ const CurrencyInput = React.forwardRef<HTMLInputElement, CurrencyInputProps>(
         thousandSeparator="."
         decimalSeparator=","
         decimalScale={2}
-        fixedDecimalScale={true}
-        valueIsNumericString={true}
+        fixedDecimalScale
+        valueIsNumericString
         onValueChange={(values, sourceInfo) => {
           if (onValueChange) {
             onValueChange(values, sourceInfo);
           }
           if (onChange) {
-            onChange(values.floatValue as any);
+            // Create a synthetic event to pass to the original onChange
+            const event = {
+              target: {
+                name: props.name,
+                value: values.value,
+              },
+            } as React.ChangeEvent<HTMLInputElement>;
+            onChange(event);
           }
-            
-          }
-          onFocus={handleFocus}
         }}
+        onFocus={handleFocus}
         {...props}
       />
     );
   }
 );
-CurrencyInput.displayName = "CurrencyInput";
+CurrencyInput.displayName = 'CurrencyInput';
 
 export { CurrencyInput };
