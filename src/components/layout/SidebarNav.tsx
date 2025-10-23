f1c5abfe756fa1699bf72dd03acbe1a20f28756a
João Vitor Machry <joaovitormachry@gmail.com>
2025-10-23 10:58:32 -0300

    Mudança no Sidebar

--- src/components/layout/SidebarNav.tsx
+++ src/components/layout/SidebarNav.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import Link from 'next/link';
 import { useAuth } from '@/hooks/use-auth';
 import { usePathname } from 'next/navigation';
@@ -37,12 +36,12 @@ export default function SidebarNav({ onAddExpenseClick, onSheetClose }: SidebarN
   const navLinks = [
     {
       href: '/dashboard',
-      label: 'Dashboard',
+      label: text.sidebar.dashboard,
       icon: LayoutDashboard,
     },
     {
       href: '/lancamentos',
-      label: 'Expenses',
+      label: text.sidebar.releases,
       icon: List,
     },
   ];
@@ -60,17 +59,6 @@ export default function SidebarNav({ onAddExpenseClick, onSheetClose }: SidebarN
       </div>
 
       {/* Content */}
-      <div className="flex-1 p-4">
-        <Button
-          onClick={handleAddExpense}
-          className="w-full justify-start text-left mb-4"
-          variant="ghost"
-          size="lg"
-        >
-          <PlusCircle className="mr-2 h-5 w-5" />
-          {text.summary.newTransaction}
-        </Button>
-
         <nav className="flex flex-col gap-1">
           {navLinks.map((link) => (
             <SheetClose asChild key={link.href}>
@@ -93,7 +81,7 @@ export default function SidebarNav({ onAddExpenseClick, onSheetClose }: SidebarN
       <div className="mt-auto border-t p-4">
         <Button onClick={handleLogout} variant="outline" className="w-full justify-start">
           <LogOut className="mr-2 h-5 w-5" />
-          Log out
+          Sair
         </Button>
       </div>
     </div>
