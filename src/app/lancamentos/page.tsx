3667385916965077f1ad9cda213fe283d07fedd5
João Vitor Machry <joaovitormachry@gmail.com>
2025-10-23 10:34:19 +0000

    Mudança na estutura

---
+++ src/app/lancamentos/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import ExpensesList from '@/components/dashboard/expenses-list';
+import { text } from '@/lib/strings';
+
+export default function LancamentosPage() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex items-center justify-between mb-6">
+        <h1 className="text-3xl font-bold">{text.dashboard.title}</h1>
+      </div>
+      <ExpensesList />
+    </div>
+  );
+}
