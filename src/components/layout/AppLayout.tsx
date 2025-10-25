d769c26d25a919ad71bf3eba63d95c295d86a53b
João Vitor Machry <joaovitormachry@gmail.com>
2025-10-25 11:26:05 +0000

    web mobile

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -33,38 +33,38 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 
   // Main layout for logged-in users
   return (
-    <div className="min-h-screen bg-background font-sans antialiased">
-       <Header
-        menuTrigger={isMobile ? (
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon" className="h-8 w-8 rounded-full">
-              <Menu className="h-4 w-4 text-muted-foreground" />
-              <span className="sr-only">Open menu</span>
-            </Button>
-          </SheetTrigger>
-        ) : undefined
-        }
-      />
-      <div className="flex">
-        {isMobile ? (
-           <Sheet>
+    <Sheet>
+      <div className="min-h-screen bg-background font-sans antialiased">
+        <Header
+          menuTrigger={isMobile ? (
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="h-8 w-8 rounded-full">
+                <Menu className="h-4 w-4 text-muted-foreground" />
+                <span className="sr-only">Open menu</span>
+              </Button>
+            </SheetTrigger>
+          ) : undefined
+          }
+        />
+        <div className="flex">
+          {isMobile ? (
             <SheetContent side="left" className="p-0 w-[80px]">
               <SidebarNav />
             </SheetContent>
-          </Sheet>
-        ) : (
-          <div className="w-[80px] border-r h-[calc(100vh-56px)]">
-             <SidebarNav />
-          </div>
-        )}
-       
-        <main className="flex-1 p-4 sm:p-6 md:p-8 h-[calc(100vh-56px)] overflow-auto border-t">{children}</main>
-      </div>
+          ) : (
+            <div className="w-[80px] border-r h-[calc(100vh-56px)]">
+              <SidebarNav />
+            </div>
+          )}
+        
+          <main className="flex-1 p-4 sm:p-6 md:p-8 h-[calc(100vh-56px)] overflow-auto border-t">{children}</main>
+        </div>
 
-      <AddExpenseForm
-        isOpen={isFormOpen}
-        onOpenChange={setIsFormOpen}
-      />
-    </div>
+        <AddExpenseForm
+          isOpen={isFormOpen}
+          onOpenChange={setIsFormOpen}
+        />
+      </div>
+    </Sheet>
   );
 }
