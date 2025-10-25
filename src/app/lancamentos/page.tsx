0d81462cf53a4bc1e02e3631edc5cfbfa508b339
João Vitor Machry <joaovitormachry@gmail.com>
2025-10-25 23:34:48 +0000

    E agora mesma coisa para a Renda Variável

--- src/app/lancamentos/page.tsx
+++ src/app/lancamentos/page.tsx
@@ -2,6 +2,7 @@
 
 import ExpensesList from '@/components/dashboard/expenses-list';
 import IncomeList from '@/components/dashboard/income-list';
+import VariableIncomeList from '@/components/dashboard/variable-income-list';
 import { Button } from '@/components/ui/button';
 import { useAddExpenseModal } from '@/contexts/AddExpenseModalContext';
 import { useAddIncomeModal } from '@/contexts/AddIncomeModalContext';
@@ -30,6 +31,7 @@ export default function LancamentosPage() {
       <div className="space-y-6">
         <ExpensesList />
         <IncomeList />
+        <VariableIncomeList />
       </div>
     </div>
   );
