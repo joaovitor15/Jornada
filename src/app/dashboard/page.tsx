ba0125afc7911f460196d18c3d716dc4365ec0dd
João Vitor Machry <joaovitormachry@gmail.com>
2025-10-27 22:43:37 +0000

    coreção espaço

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -70,7 +70,7 @@ export default function DashboardPage() {
 
   return (
     <>
-      <div className="container mx-auto p-4">
+      <div className="p-2">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
           <div className="lg:col-span-2">
             <FinancialChart />
