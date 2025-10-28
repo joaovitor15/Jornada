37ba786e4ec58c6ef3c1492141536db0c9413289
João Vitor Machry <joaovitormachry@gmail.com>
2025-10-28 14:49:37 +0000

    atual

--- src/lib/strings.ts
+++ src/lib/strings.ts
@@ -1,7 +1,7 @@
 export const text = {
   header: {
     appName: 'A Jornada',
-    appDescription: 'Sua jornada de controle Financeiro',
+    appDescription: 'Sua jornada do controle Financeiro',
     profiles: 'Perfis',
     personal: 'Joao',
     home: 'Casa',
