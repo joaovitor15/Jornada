6266024164849a22b2022db40a922eb6f0d41eb5
João Vitor Machry <joaovitormachry@gmail.com>
2025-10-28 11:11:06 +0000

    propoção grafico

--- src/components/dashboard/FinancialChart.tsx
+++ src/components/dashboard/FinancialChart.tsx
@@ -153,7 +153,7 @@ export default function FinancialChart() {
   }
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer width="100%" height={320}>
       <AreaChart data={chartData} margin={{ top: 5, right: 20, left: -10, bottom: 20 }}>
         <defs>
           <linearGradient id="colorIncome" x1="0" y1="0" x2="0" y2="1">
