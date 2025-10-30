800e4989a5a6a9a6628dd93ed0d95762182603ae
João Vitor Machry <joaovitormachry@gmail.com>
2025-10-30 13:50:05 +0000

    podemos dar sequencia

--- src/lib/categories/business.ts
+++ src/lib/categories/business.ts
@@ -1,18 +1,18 @@
 export const businessExpenseCategories = {
-   "Fornecedores": ["Pagamentos Fonecedores"], 
-   "Funcionários": ["João Vitor", "Mari Lucia Dama", "Lucas", "Lisi", "Aline", "Jesica", "Eliza"],
-   "Conta Consumo": ["Água", "Luz (SA)", "Luz (AP1)"],
-   "Sistema": ["BrPhonia", "Trier Sistemas", "SKY TEF"],
-   "PBM": ["Portal Drogaria", "Funcional", "Portal E-pharma"],
-   "Agafarma": ["Mensalidade", "Cartão"],
-   "Maketing": ["Rádio", "ACI", "Outros"],
-   "Escritorio": ["Lider", "Serplamed", "Certificado Digital"],
-   "Impostos": ["FGTS", "Receitas (INSS + IRPF)", "Simples Nacional"],
-   "Fiscalização" : ["CRF/RS", "Certidão Simplificada", "ARL (Lixos)"]
-
-    
+  "Fornecedores": ["Pagamentos Fonecedores"],
+  "Funcionários": ["João Vitor", "Mari Lucia Dama", "Lucas", "Lisi", "Aline", "Jesica", "Eliza"],
+  "Conta Consumo": ["Água", "Luz (SA)", "Luz (AP1)"],
+  "Sistema": ["BrPhonia", "Trier Sistemas", "SKY TEF"],
+  "PBM": ["Portal Drogaria", "Funcional", "Portal E-pharma"],
+  "Agafarma": ["Mensalidade", "Cartão"],
+  "Maketing": ["Rádio", "ACI", "Outros"],
+  "Escritorio": ["Lider", "Serplamed", "Certificado Digital"],
+  "Impostos": ["FGTS", "Receitas (INSS + IRPF)", "Simples Nacional"],
+  "Fiscalização": ["CRF/RS", "Certidão Simplificada", "ARL (Lixos)"],
+  "Ajustes": ["Saída (Transferência)"],
 };
 
 export const businessIncomeCategories = {
-    "Recebimentos": ["Dinheiro", "Farmacia Popular (gov)"],
+  "Recebimentos": ["Dinheiro", "Farmacia Popular (gov)"],
+  "Ajustes": ["Entrada (Transferência)"],
 };
