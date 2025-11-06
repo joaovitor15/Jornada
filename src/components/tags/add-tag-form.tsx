9d2c8ad3c19c9d17aa296f2fe7a9c918b4dbb754
João Vitor Machry <joaovitormachry@gmail.com>
2025-11-06 18:37:21 +0000

    e só mais uma coisa gostaria de remover o simbolo do nordpass pois ele n

--- src/components/tags/add-tag-form.tsx
+++ src/components/tags/add-tag-form.tsx
@@ -241,6 +241,7 @@ export default function AddTagForm({
                       }
                       {...field}
                       disabled={isSubmitting}
+                      autoComplete="off"
                     />
                   </FormControl>
                   <FormMessage />
