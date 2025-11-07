dda6d9e3423730cd262d50abe992f0ec668741f1
João Vitor Machry <joaovitormachry@gmail.com>
2025-11-07 22:19:02 +0000

    Sim, somente nos dois perfis e isso não vai mudar coisa externa, isso eu

--- src/app/relatorios/page.tsx
+++ src/app/relatorios/page.tsx
@@ -406,42 +406,6 @@ export default function ReportsPage() {
        </div>
    );
  }
-     return (
-        <div className="p-4 md:p-6 lg:p-8 lg:pt-4">
-          {commonHeader}
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-            <div className="lg:col-span-2 space-y-6">
-              <Card>
-                <CardHeader>
-                  <CardTitle>
-                    {text.reports.financialSummary(selectedYear)}
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <AnnualFinancialChart year={selectedYear} onMonthSelect={setSelectedMonth} />
-                </CardContent>
-              </Card>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                 <CategoryCardSpendingTabs 
-                    selectedMonth={selectedMonth} 
-                    selectedYear={selectedYear}
-                    showCardSpending={activeProfile === 'Personal'}
-                />
-                <IncomeAnalysisTabs 
-                    selectedMonth={selectedMonth} 
-                    selectedYear={selectedYear}
-                />
-              </div>
-            </div>
-            <div className="lg:col-span-1 space-y-6">
-                <HomeAndPersonalCards 
-                    selectedMonth={selectedMonth}
-                    selectedYear={selectedYear}
-                />
-            </div>
-          </div>
-        </div>
-    );
 
   const isLoading = dataLoading;
 
